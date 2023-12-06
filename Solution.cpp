4648f8a6e5ae383d9a2f55d5b71687e473cba614
Raghav Miglani <raghavmiglani7@gmail.com>
2023-12-06 14:02:41 +0530

    Running Testcases

--- Solution.cpp
+++ Solution.cpp
@@ -75,3 +75,45 @@ int main()
         cout <<"]\n";
     }
 }
+
+/*
+
+Testcase 1 
+
+Input 
+
+init 12
+reg 1 B V
+reg 2 A V 
+reg 3 A V 
+reg 4 B NV 
+reg 5 B V 
+reg 6 A NV 
+reg 7 A V 
+reg 8 A NV 
+reg 9 B NV 
+reg 10 B V 
+reg 11 A NV 
+reg 11 A NV
+reg 12 B NV 
+reg 13 A NV 
+fin
+
+
+Expected Output:
+
+ANV : [6,8,11] 
+AV : [2,3,7]
+BNV : [4,9,12] 
+BV : [1,5,10]
+NA : [13]
+
+Received Output:
+
+ANV : [6,8,11]
+AV : [2,3,7]
+BNV : [4,9,12]
+BV : [1,5,10]
+NA : [13]
+
+*/
